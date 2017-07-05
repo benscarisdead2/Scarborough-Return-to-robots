8dc58ad0f51dac5d3e155b0d7b1115ae0ee9cc40
ben scarborough <benscarborough@scarflix.com>
2017-07-05 16:01:06 -0400

    Inital commit

---
+++ routes/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const mongo = require("mongodb").MongoClient;
+const databaseUrl = "mongodb://localhost:27017/robotsdb2";
+
+
+
+
+
+module.exports = router;
